0db82ca1e6d6d141ef77970ff9a1b5afabee7cfe
Jose Vargas <arnoldovargas1981@gmail.com>
2022-05-10 13:36:58 -0400

    Aqui comienza

---
+++ src/main/java/com/bicom/academy/BacademyApplication.java
@@ -0,0 +1,13 @@
+package com.bicom.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacademyApplication.class, args);
+	}
+
+}
